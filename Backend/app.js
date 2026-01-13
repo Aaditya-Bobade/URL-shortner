ed176f6eae1f56235b095ea053bafc7b0360826b
Aaditya Bobade <aadityabobade17@gmail.com>
2026-01-13 22:51:18 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://url-shortner-60cf.onrender.com', 
     credentials: true 
 }));
 
